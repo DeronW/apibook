b3caae01700fd7a8f4625e15076b3866e6056669
Deron.W <w.del@qq.com>
2018-06-09 22:08:36 +0800

    I'm too lazy, nothing left

--- server/server/settings.py
+++ server/server/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'assets')
+]
 STATIC_ROOT = './static'
 STATIC_URL = '/static/'
 
