03c93472273fae71bc9f6fb05eccdfb5951a8f19
Deron.W <w.del@qq.com>
2018-06-17 22:16:21 +0800

    brands homepage

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
-        Hello, Kitty
-    </div>
+
+    <v-layout justify-center align-center>
+        <h1 style="font-size: 68px; text-align: center; color: #e0e0e0;">API Book</h1>
+    </v-layout>
 </template>
