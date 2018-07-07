3d7e7293baa363cce1fdfab0f2f360189d105ebb
Deron.W <w.del@qq.com>
2018-07-07 18:38:54 +0800

    I'm too lazy, nothing left

---
+++ nuxt/pages/api/index.vue
@@ -0,0 +1,20 @@
+<script src="./api.js"></script>
+<i18n>
+{}
+</i18n>
+<template>
+<v-layout row wrap>
+    <v-flex xs6>
+        form
+    </v-flex>
+    <v-flex xs6>
+        header
+    </v-flex>
+    <v-flex xs6>
+        request
+    </v-flex>
+    <v-flex xs6>
+        response
+    </v-flex>
+</v-layout>
+</template>
